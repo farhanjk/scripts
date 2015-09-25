150df630a48087116984d2a9db663beda25ae7ca
Farhan Khan <farhanjk@gmail.com>
2015-09-24 22:46:16 -0700

    Vim, tmux copy script

---
+++ brew_installs.sh
@@ -0,0 +1,4 @@
+# http://unix.stackexchange.com/questions/15715/getting-tmux-to-copy-a-buffer-to-the-clipboard
+# copy: <tmux-key> Control-c -> Command-v or pbpaste
+# paste: <tmux-key> Control-v
+brew install reattach-to-user-namespace
