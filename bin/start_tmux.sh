bbda633c777e8ced1c7f917e04e0b7c571eba6b1
Farhan Khan <farhanjk@gmail.com>
2015-09-25 18:36:14 -0700

    Subscript to move to myo2 db folder

--- bin/start_tmux.sh
+++ bin/start_tmux.sh
@@ -2,6 +2,7 @@
 
 killall tmux
 tmux new -s MyO2Android -d
+tmux send-keys -t MyO2Android:1.0 'cd ~/scripts; clear; pwd; ls' C-m
 tmux new-window -n MyO2_repo
 tmux send-keys -t MyO2Android:2.0 'echo; cd ~/MyO2Android; ls; echo; git branch -v; echo; git status' C-m
 tmux new-window -n iOS_repo
